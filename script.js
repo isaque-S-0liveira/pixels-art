54f1a05312e71265b8943c9db4507a90c82e3082
Isaque <zazac3179@gmail.com>
2022-08-04 20:53:35 -0300

    corrigindo erros

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function quadroDePixels() {
     for (let i = 0; i < 25; i += 1) {
         const pixels = document.createElement('div');
         pixels.className = 'pixel';
-        pixels.style.backgroundColor = 'wite';
+        pixels.style.backgroundColor = 'white';
         pixelBoard.appendChild(pixels);
     }
 
